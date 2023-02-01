4c2dcd71e6550db3eb8c9a7de48404d2834c7bbb
girishgulhan <90096438+girishgulhan@users.noreply.github.com>
2023-02-01 10:04:32 +0530

    added;

---
+++ src/main/java/com/example/BankingAppPostgres/ProjectService.java
@@ -0,0 +1,19 @@
+package com.example.BankingAppPostgres;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+	
+@Autowired
+private ProjectRepository repo;
+
+  public Iterable<Project> getAllprojects() {
+	  
+	  return repo.findAll();
+	  
+	 
+  }
+
+}
---
+++ src/main/java/com/example/BankingAppPostgres/StudentService.java
@@ -0,0 +1,28 @@
+package com.example.BankingAppPostgres;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	
+	public StudentRepository repo;
+
+	 public Iterable<Student> getStudentDetails()
+	 {
+		 return repo.findAll();
+	 }
+	 
+	 public boolean deleteStudentDetails(Student t)
+	 {
+		  repo.delete(t);
+		  
+		  return true;
+	 }
+	 
+	 public void saveStudents(Student t) {
+		 repo.save(t);
+	 }
+}
